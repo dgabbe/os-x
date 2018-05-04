50a0765f06c01826b216a4f88069eb1677efab7f
David G <dgabbe@acm.org>
2018-05-04 07:49:17 -0400

    Saving end of week work.

--- os-x-config/standard_tweaks/info.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-echo 'MacOS:' `sw_vers -productVersion`
-echo 'Python:' `python -V`
-echo 'which python:' `which python`
-echo 'whereis python:' `whereis python`
-echo 'which python3:' `which python3`
-echo 'privs:' `id -Gn`
