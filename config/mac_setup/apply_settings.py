051b38c254402fe678d6be3c17f3976e8c7c592c
David G <dgabbe@acm.org>
2019-04-08 13:00:49 -0400

    Saving changes;

--- config/mac_setup/apply_settings.py
+++ config/mac_setup/apply_settings.py
@@ -1,8 +1,61 @@
+#! /usr/bin/env python3
+
+#
+# Resources:
+#   - https://developer.apple.com/documentation/foundation/nsuserdefaults
+#   - https://github.com/mathiasbynens/dotfiles/
+#   - https://github.com/paulirish/dotfiles
+#
+
+from argparse import ArgumentParser
 from csv import DictReader, register_dialect
 from os.path import dirname, join
+from sys import exit, stderr
 
-register_dialect("comma-space", delimiter=",", skipinitialspace=True)
-with open(join(dirname(__file__), "settings/defaults.csv"), newline="") as csvfile:
-    reader = DictReader(csvfile, dialect="comma-space")
-    for row in reader:
-        print(row)
+
+def is_admin():
+    """Check to see if the user belongs to the 'admin' group.
+
+    :return: boolean
+    """
+    return os.getlogin() in grp.getgrnam("admin").gr_mem
+
+
+def main():
+    parser = ArgumentParser(
+        prog="Apply MacOS Settings",
+        description="""Tailor MacOS settings for better performance and better default behavior""",
+        epilog="""Only settings that need changing need are executed. Changes are logged to <<fill in>>. 
+        The format for the csv file is <<fill in later>>""",
+    )
+    parser.add_argument(
+        "-settings",
+        choices=["defaults", "shell", "sudo", "all"],
+        default="defaults",
+        help="Which group of settings to apply.",
+    )
+    parser.add_argument(
+        "-dryrun",
+        action="store_true",
+        default=False,
+        help="Simulate and report the changes, but do not make them.",
+    )
+    parser.add_argument(
+        "-quiet", action="store_true", default=False, help="""Operate in quiet mode."""
+    )
+    args = parser.parse_args()
+
+    register_dialect("comma-space", delimiter=",", skipinitialspace=True)
+    with open(join(dirname(__file__), "settings/defaults.csv"), newline="") as csvfile:
+        reader = DictReader(csvfile, dialect="comma-space")
+        row_count = sum(1 for row in csvfile)
+        print("    Reviewing {} settings...".format(row_count - 1))
+        csvfile.seek(0)
+        for row in reader:
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    print("    ** Did not plan on being imported **", file=stderr)
--- config/mac_setup/commands.py
+++ config/mac_setup/commands.py
@@ -1,17 +1,6 @@
-# test = {'group': 'general',
-#         'type': 'user', # [user | system]
-#         'method': 'command', # [command | shell]
-#         'description': 'The keyboard react faster to keystrokes (not equally useful for everyone)',
-#         'command': 'defaults',
-#         'domain_key': 'NSGlobalDomain KeyRepeat',
-#         'preferred_value': '-int 0',
-#         'set': 'write',
-#         'get': 'read',
-#         'os_v_min': None, 'os_v_max': None
-#         }
-#import dglogger
 import shlex
 import subprocess
+
 
 class cmd:
     """
@@ -19,20 +8,17 @@ class cmd:
     """
 
     def __init__(self):
-        self.group = 'general'
-        self.type = 'user'
-        self.method = 'command'
+        self.method = "command"
         self.description = None
-        self.source = None
-        self.os_min = None
-        self.os_max = None
 
     def __repr__(self):
-        return (f'{self.type}, {self.group}, {self.method}' # figure out syntax and add text 
-                f'{self.__class__.__name__}('
-                f'{self.group!r}, {self.description!r})')
+        return (
+            f"{self.method}"  # figure out syntax and add text
+            f"{self.__class__.__name__}("
+            f"{self.description!r})"
+        )
 
-    def os_set_cmd(self, cmd):
+    def set(self, cmd):
         # try
         # logging
         # if command or shell or sudo
@@ -40,45 +26,86 @@ class cmd:
 
 
 class defaults_cmd(cmd):
-    """For preferences set with 'defaults' command"""
+    """For 'defaults' command settings."""
 
-    def __init__(self, description, source):
+    def __init__(self, domain, domain_key, value_type, value, description):
         """Create a new instance of a defaults preference.
-        Must provide a domain key and value.
+        Must provide a domain, domain key and value.
         """
         super().__init__()
         self.description = description
-        self.source = source
-        self.command = 'defaults'
-        self.set = 'write'
-        self.get = 'read'
+        self.command = "defaults"
+        self.set = "write"
+        self.get = "read"
+        self.domain = domain
+        self.domain_key = domain_key
+        self.value_type = value_type
+        self.value = value
         try:
-            ss = source.split()
-            self.domain_key = ss[2] + " " + ss[3]
-            self.preferred_value = ss[4] + " " + ss[5]
+            pass
         except IndexError:
-            dglogger.log_warning("Expecting domain key and value for: " + self.source)
-#            raise ValueError
-#            self.__del__()
+            pass
 
+    #            raise ValueError
+    #            self.__del__()
 
-    def os_set_cmd(self):
+    def convert_bool_type(bool_value):
+        if bool_value in ("0", "NO", "FALSE"):
+            "0"
+        elif bool_value in ("1", "TRUE", "YES"):
+            "1"
+        else:
+            pass
+            # raise convert_bool_type error
+
+    def validate_value_type(self):
+        # from man page
+        # value types are: -string, -data, -int[eger], -float, -bool[ean], -date, -array-add, -dict, -dict-add
+        if self.value_type in (
+            "-string",
+            "-data",
+            "-int",
+            "-integer",
+            "-float",
+            "-date",
+        ):
+            pass
+            # just store value
+        elif self.value_type in ("-bool", "-boolean"):
+            pass
+            # convert value into 0 or 1
+        elif self.value_type in ("-date", "-array-add", "-dict", "-dict-add"):
+            pass
+            # raise unsupported exception
+        else:
+            pass
+            # raise bad value type error
+
+    #    def __get__(self, instance, owner):(self):
+
+    def get_cmd(self):
+        """The actual defaults command string to retrieve"""
+
+        c = self.command + self.get + self.domain_key
+        super().os_set_cmd(self, c)
+
+    def set_cmd(self):
         """The actual defaults command string to execute"""
 
         c = self.command + self.set + self.domain_key + self.preferred_value
         super().os_set_cmd(self, c)
 
 
-class shell_cmd(cmd):
-    """For shell commands (sh or bash)"""
-
-    def __init__(self, description, source):
-        """Create a shell command"""
-        super().__init__()
-        self.method = 'shell'
-        self.description = description
-        self.source = source
-        self.command = " ".join(shlex(self.source))
-
-    def os_set_cmd(self):
-        print('Under development: ', self.command)
+# class shell_cmd(cmd):
+#     """For shell commands (sh or bash)"""
+#
+#     def __init__(self, description, source):
+#         """Create a shell command"""
+#         super().__init__()
+#         self.method = 'shell'
+#         self.description = description
+#         self.source = source
+#         self.command = " ".join(shlex(self.source))
+#
+#     def os_set_cmd(self):
+#         print('Under development: ', self.command)
