d791ba5da57e4db50660723e1433dcf23b0a5c90
David G <dgabbe@acm.org>
2019-09-24 05:42:00 -0400

    Updated for new build directory structure.

--- config/mac_setup/bin/preqs.sh
+++ config/mac_setup/bin/preqs.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 # think about moving to bin for global access
-pipreqs --print . --ignore _untracked,apply_settings.dist,macos_settings.app,macos_settings.dist,nuitka
+pipreqs --print . --ignore _untracked,app,macos_settings.dist,nuitka,bin
