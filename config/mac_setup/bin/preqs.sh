d5ea8cd7ab8c6a05bbe85183d8e3b2f635963c92
David G <dgabbe@acm.org>
2021-01-05 19:05:31 -0500

    Moved to project under _python.

--- config/mac_setup/bin/preqs.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-# think about moving to bin for global access
-pipreqs --print . --ignore _untracked,app,macos_settings.dist,nuitka,bin
--- config/mac_setup/main.py
+++
@@ -1,9 +0,0 @@
-# python3 -m nuitka --follow-imports --show-progress --python-flag=no_site --remove-output  main.py --standalone
-
-from mac_setup.macos_settings import main
-from sys import stderr
-
-if __name__ == "__main__":
-    main()
-else:
-    print("** Did not plan on being imported **", file = stderr)
